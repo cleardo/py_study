5e10aff7c0f9c6aef62c4bdb6b68518be6458294
cleardo <cleardo@yeah.net>
2015-10-08 15:18:51 +0800

    确定性状态机

--- lex/dfa.py
+++ lex/dfa.py
@@ -5,3 +5,47 @@ class Dfa(object):
     """
     确定状态机
     """
+    def __init__(self):
+        """
+
+        :return:
+        """
+
+        # 状态数
+        self.nstates = 0
+
+        # 状态转换表数组指针
+        self.dfap = list()
+
+        # 接受状态数组指针
+        self.acceptp = list()
+
+    def set_dfa(self):
+        """
+        设置转换表
+
+        :return:
+        """
+
+    def set_accept(self):
+        """
+        设置接受状态数组
+
+        :return:
+        """
+
+    def is_accepted(self, s):
+        """
+        判断指定状态是否是被接受的
+
+        :return:
+        """
+
+    def make_dfa(self):
+        """
+        构造一个确定性状态机
+
+        :return:
+        """
+        return self.nstates
+
