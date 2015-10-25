6c08f882972bbf5cfc946650e63dd3a588350a4f
cleardo <cleardo@yeah.net>
2015-10-25 22:34:37 +0800

    邮箱发送测试

--- flask_test/upload.py
+++ flask_test/upload.py
@@ -1,7 +1,13 @@
-# coding = utf-8
+# -*- coding: UTF-8 -*-
+import os
+
+from flask.ext.wtf import Form
+from werkzeug.utils import secure_filename
+from wtforms import StringField, FileField, SubmitField
+
 __author__ = 'donglin'
 
-from flask import Flask
+from flask import Flask, render_template, request
 
 from flask.ext.script import Manager
 
@@ -9,8 +15,9 @@ from flask.ext.mail import Message
 
 from flask.ext.mail import Mail
 
-app = Flask(__name__)
+from flask.ext.bootstrap import Bootstrap
 
+app = Flask(__name__)
 
 app.config['SECRET_KEY'] = "cleardoa*b(c%225588"
 
@@ -28,10 +35,30 @@ app.config['MAIL_PASSWORD'] = "2216593flik"
 
 mail = Mail(app)
 manager = Manager(app)
+bootstrap = Bootstrap(app)
 
+class UploadForm(Form):
+    name = StringField("file")
+    up_file = FileField(u"上传")
+    submit = SubmitField(u"提交")
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    # view中调用wtf.quick_form()
+    form = UploadForm()
+
+    if form.validate_on_submit():
+        file = request.files['up_file']
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        pass
+        print "ok"
+
+    # render_template()
+    return render_template("upload.html", form=form)
+
+@app.route('/mail')
+def mail():
     msg = Message("test subject", sender="hong2516@163.com", recipients=["cleardo@yeah.net"])
 
     msg.body = "text body"
