25c0943462f76ea105d73caf79ff9b8400622839
cleardo <cleardo@yeah.net>
2015-11-03 22:07:42 +0800

    邮箱发送测试

--- flask_test/test01.py
+++ flask_test/test01.py
@@ -1,9 +1,12 @@
 # coding=utf-8
+from flask.ext.restful import reqparse
+import werkzeug
 
 __author__ = 'donglin'
 
 
-from flask import Flask
+from flask import Flask, request
+
 # 引入flask restful框架
 from flask_restful import Resource, Api
 
@@ -18,6 +21,33 @@ class HelloWorld(Resource):
     def get(self):
         # 从数据库获取数据，然后填充dict
         return {"hello": "world"}
+
+    def post(self):
+        # print request.get_json(force=True)
+        parser = reqparse.RequestParser()
+
+        # 表单提交
+        parser.add_argument('rate', type=int, help='Rate cannot be converted')
+
+        parser.add_argument('name')
+
+        parser.add_argument('picture', type=werkzeug.datastructures.FileStorage, location='files')
+
+        args = parser.parse_args()
+
+        # 文件存储
+        # args['picture']
+
+        print args
+        # print request.args
+        print request.form['rate']
+
+        # print request.data
+        # print request.form
+        # print request.args
+
+        return {"hello": "world"}
+
 
 # 对资源定义'/'
 api.add_resource(HelloWorld, '/')
